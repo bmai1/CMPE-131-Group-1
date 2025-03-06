c8c73685e7b986f2323f031a68725f83d8684ca7
Brian Mai <0hrs3g@gmail.com>
2025-03-06 02:08:00 -0800

    Init database

--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,4 @@
-CREATE TYPE account {
-    account_type varchar(255) NOT NULL,
-    balance INT(255) NOT NULL,
-}
-
-CREATE TABLE users {
-    username varchar(255) NOT NULL UNIQUE,
-    password varchar(255) NOT NULL,
-};
+CREATE TABLE users (
+    username TEXT NOT NULL UNIQUE,
+    password BLOB NOT NULL
+);
