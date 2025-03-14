e57c3af1bbc9e43bbdbf94fed56d030525966a85
Brian Mai <0hrs3g@gmail.com>
2025-03-13 18:41:31 -0700

    Load dotenv

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import sqlite3
 import bcrypt
 import os
 from flask import Flask, render_template, g, request, redirect, url_for, session
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
